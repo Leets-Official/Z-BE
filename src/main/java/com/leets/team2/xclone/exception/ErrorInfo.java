01c4f487f07915225dea161979a499d2223c5f38
soyesenna <85686722+soyesenna@users.noreply.github.com>
2024-10-07 23:42:53 +0900

    [feat] 표준 응답객체 구현 및 표준 에러 정의 (#7)
    * feat: 에러를 표준화할 enum 정의
    
    * feat: 에러 응답을 표준화하기위한 예외 클래스 구현
    
    * feat: 표준 응답객체 구현 및 GlobalExceptionHandler구현
    
    * feat: 표준 응답 객체에 created 편의 메서드 추가
    
    * chore: spring starter validation 의존성 추가

---
+++ src/main/java/com/leets/team2/xclone/exception/ErrorInfo.java
@@ -0,0 +1,20 @@
+package com.leets.team2.xclone.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorInfo {
+
+  // validation 실패
+  NOT_VALID_REQUEST(HttpStatus.BAD_REQUEST, "요청을 검증하는데 실패했습니다.", 9999),
+
+  // Member 영역
+  NO_SUCH_MEMBER(HttpStatus.NOT_FOUND, "해당 멤버를 찾을 수 없습니다.", 10001);
+
+  private final HttpStatus statusCode;
+  private final String message;
+  private final int code;
+}
---
+++ src/main/java/com/leets/team2/xclone/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,44 @@
+package com.leets.team2.xclone.exception.handler;
+
+import com.leets.team2.xclone.common.ApiData;
+import com.leets.team2.xclone.exception.ApplicationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+  private static final String INTERNAL_SERVER_ERROR = "서버에 오류가 발생했습니다.\n잠시후 다시 시도해주세요.";
+
+  @ExceptionHandler(ApplicationException.class)
+  public ResponseEntity<ApiData<String>> handleApplicationException(ApplicationException e) {
+    return ApiData.errorFrom(e.getErrorInfo());
+  }
+
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  @ExceptionHandler(RuntimeException.class)
+  public String handleAnyRunTimeException(RuntimeException e) {
+    log.warn("Unexpected Error Occurred", e);
+    return INTERNAL_SERVER_ERROR;
+  }
+
+
+
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  @ExceptionHandler(Exception.class)
+  public String handleAnyCheckedException(Exception e) {
+    log.error("Unexpected Error Occurred", e);
+    return INTERNAL_SERVER_ERROR;
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ApiData<String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    return ApiData.validationFailure(e.getFieldErrors());
+  }
+}
