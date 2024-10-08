dea8d2bb2132d120a9b2ed3361ba2b7617f3426c
senna <kjy915875@gmail.com>
2024-10-08 22:11:24 +0900

    docs: OAuth관련 스웨거 작성

--- src/main/java/com/leets/team2/xclone/domain/auth/controller/AuthController.java
+++ src/main/java/com/leets/team2/xclone/domain/auth/controller/AuthController.java
@@ -1,12 +1,24 @@
 package com.leets.team2.xclone.domain.auth.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.leets.team2.xclone.common.ApiData;
+import com.leets.team2.xclone.domain.auth.dto.response.OAuthLoginResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import org.springframework.http.ResponseEntity;
 
 @Tag(name = "Authentication", description = "Auth 관련 API")
 public interface AuthController {
 
+  @Operation(summary = "카카오 OAuth2 로그인 시작 엔드포인트", description = "카카오 로그인 시작 시 이 엔드포인트로 GET 요청을 보내서 카카오 로그인 페이지로 Redirect합니다.")
   void getLogin(HttpServletResponse httpServletResponse) throws IOException;
 
+  @Operation(summary = "카카오 OAuth2 Redirect 엔드포인트", description = "카카오에서 받은 인가 코드를 전송하여 로그인을 진행합니다.")
+  @ApiResponse(responseCode = "200", description = "카카오 OAuth2 로그인 성공", content = @Content(schema = @Schema(implementation = OAuthLoginResponse.class)))
+  ResponseEntity<ApiData<OAuthLoginResponse>> getRedirect(String code) throws JsonProcessingException;
 }
--- src/main/java/com/leets/team2/xclone/domain/auth/dto/response/OAuthLoginResponse.java
+++ src/main/java/com/leets/team2/xclone/domain/auth/dto/response/OAuthLoginResponse.java
@@ -1,7 +1,11 @@
 package com.leets.team2.xclone.domain.auth.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record OAuthLoginResponse(
+    @Schema(description = "최초 로그인 여부, 최초 로그인이면 회원가입 페이지로 이동해야합니다.", example = "true")
     Boolean requiredRegister,
+    @Schema(description = "멤버의 닉네임, 모든 응답에 반환됩니다.", example = "kimjooyoung")
     String nickname
 ) {
 
