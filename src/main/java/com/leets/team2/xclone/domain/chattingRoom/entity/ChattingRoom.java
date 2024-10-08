89ec26b585357d7e2e269a2f43af1636184a4cbf
jj <syeonie0526@gmail.com>
2024-10-08 22:43:09 +0900

    refactor: 채팅방 엔티티 어노테이션 추가

--- src/main/java/com/leets/team2/xclone/domain/chattingRoom/entity/ChattingRoom.java
+++ src/main/java/com/leets/team2/xclone/domain/chattingRoom/entity/ChattingRoom.java
@@ -3,7 +3,14 @@ package com.leets.team2.xclone.domain.chattingRoom.entity;
 import com.leets.team2.xclone.common.entity.AbstractEntity;
 import com.leets.team2.xclone.domain.member.entities.Member;
 import jakarta.persistence.*;
+import lombok.*;
 
+@Entity
+@Table(name = "chatting_room")
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
 public class ChattingRoom extends AbstractEntity {
     @ManyToOne
     @JoinColumn(name = "member1_id")
