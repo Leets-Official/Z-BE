57943b0380654ac9f113854b94eb82fc76dc1c3c
senna <kjy915875@gmail.com>
2024-10-08 23:02:14 +0900

    refactor: Member 엔티티에서 전화번호 삭제

--- src/main/java/com/leets/team2/xclone/domain/member/entities/Member.java
+++ src/main/java/com/leets/team2/xclone/domain/member/entities/Member.java
@@ -19,9 +19,6 @@ import lombok.NoArgsConstructor;
 @Getter
 public class Member extends AbstractEntity {
 
-  @Column(name = "phone_number", nullable = false)
-  private String phoneNumber;
-
   @Column(name = "birth_date", nullable = true)
   private LocalDateTime birthDate;
 
