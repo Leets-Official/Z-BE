b2ebe08e3cc22bfef64b88a307dcffe315230150
senna <kjy915875@gmail.com>
2024-10-08 20:31:31 +0900

    feat: application 관련 설정을 가져오는 클래스 추가

---
+++ src/main/java/com/leets/team2/xclone/config/ConfigProperties.java
@@ -0,0 +1,15 @@
+package com.leets.team2.xclone.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "app")
+@Getter
+@Setter
+public class ConfigProperties {
+
+  private String baseUrl;
+}
