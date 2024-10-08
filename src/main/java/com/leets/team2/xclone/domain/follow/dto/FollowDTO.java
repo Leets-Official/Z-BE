dd76e0693fa249c4edf7c98ae2c4cf8aee7e4060
jj <syeonie0526@gmail.com>
2024-10-08 19:35:24 +0900

    refactor: 빌더 어노테이션 추가

--- src/main/java/com/leets/team2/xclone/domain/follow/dto/FollowDTO.java
+++ src/main/java/com/leets/team2/xclone/domain/follow/dto/FollowDTO.java
@@ -1,13 +1,16 @@
 package com.leets.team2.xclone.domain.follow.dto;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 
 public class FollowDTO {
 
+    @Builder
     public record Save(
             @NotNull String tag
     ){}
 
+    @Builder
     public record Response(
             Long id,
             String tag,
