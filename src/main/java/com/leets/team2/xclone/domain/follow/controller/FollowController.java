baba62c396e90d316196b4ef88e865cd9b1e08d0
jj <syeonie0526@gmail.com>
2024-11-04 17:27:49 +0900

    feat: follower 조회 기능 추가

--- src/main/java/com/leets/team2/xclone/domain/follow/controller/FollowController.java
+++ src/main/java/com/leets/team2/xclone/domain/follow/controller/FollowController.java
@@ -1,4 +1,25 @@
 package com.leets.team2.xclone.domain.follow.controller;
 
+import com.leets.team2.xclone.common.ApiData;
+import com.leets.team2.xclone.domain.follow.dto.FollowDTO;
+import com.leets.team2.xclone.domain.follow.service.FollowService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/follows")
 public class FollowController {
+    private final FollowService followService;
+    @GetMapping("/followers")
+    public ResponseEntity<ApiData<List<FollowDTO.Response>>> getFollowersByTag(@RequestParam String tag){
+        List<FollowDTO.Response> followers = followService.getFollowers(tag);
+        return ApiData.ok(followers);
+    }
 }
--- src/main/java/com/leets/team2/xclone/domain/follow/service/FollowService.java
+++ src/main/java/com/leets/team2/xclone/domain/follow/service/FollowService.java
@@ -1,11 +1,31 @@
 package com.leets.team2.xclone.domain.follow.service;
 
+import com.leets.team2.xclone.domain.follow.dto.FollowDTO;
+import com.leets.team2.xclone.domain.follow.entity.Follow;
 import com.leets.team2.xclone.domain.follow.repository.FollowRepository;
+import com.leets.team2.xclone.domain.member.entities.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class FollowService {
     private final FollowRepository followRepository;
+
+    public List<FollowDTO.Response> getFollowers(String tag){
+        List<Follow> followers = followRepository.findByFollowee_Tag(tag);
+        return followers.stream()
+                .map(follow -> {
+                    Member follower = follow.getFollower();
+                    return FollowDTO.Response.builder()
+                            .id(follower.getId())
+                            .tag(follower.getTag())
+                            .nickname(follower.getNickname())
+                            .build();
+                })
+                .collect(Collectors.toList());
+    }
 }
