c5d2967b86ff6a233af01c0d57f66ee268889ef1
jj <syeonie0526@gmail.com>
2024-11-05 01:30:30 +0900

    refactor: INVALID_FOLLOW의 에러 메세지 변경

--- src/main/java/com/leets/team2/xclone/exception/ErrorInfo.java
+++ src/main/java/com/leets/team2/xclone/exception/ErrorInfo.java
@@ -19,7 +19,7 @@ public enum ErrorInfo {
   INVALID_TOKEN(HttpStatus.BAD_REQUEST, "잘못된 토큰입니다.", 10100),
 
   // follow 영역
-  INVALID_FOLLOW(HttpStatus.BAD_REQUEST, "잘못된 팔로우 요청입니다.", 10201),
+  INVALID_FOLLOW(HttpStatus.BAD_REQUEST, "잘못된 팔로우 요청입니다. (자기 자신을 팔로우하거나 언팔로우할 수 없습니다.)", 10201),
   FOLLOW_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "이미 팔로우한 대상입니다", 10202),
   NO_SUCH_FOLLOW(HttpStatus.BAD_REQUEST, "팔로우하지 않은 대상입니다", 10203);
 
