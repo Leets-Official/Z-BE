eb5ed8db41c49d0301c53e7405f2b65bde2a136c
soyesenna <85686722+soyesenna@users.noreply.github.com>
2024-11-13 22:50:22 +0900

    feat: 내 태그 가져오기 API 구현 (#60)

--- src/main/java/com/leets/team2/xclone/domain/member/controller/MemberController.java
+++ src/main/java/com/leets/team2/xclone/domain/member/controller/MemberController.java
@@ -6,6 +6,7 @@ import com.leets.team2.xclone.domain.member.dto.requests.CheckTagDuplicationGetR
 import com.leets.team2.xclone.domain.member.dto.requests.MemberFindGetRequest;
 import com.leets.team2.xclone.domain.member.dto.responses.CheckTagDuplicationGetResponse;
 import com.leets.team2.xclone.domain.member.dto.responses.MemberFindGetResponse;
+import com.leets.team2.xclone.domain.member.dto.responses.MemberTagGetResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -28,4 +29,6 @@ public interface MemberController {
   @Operation(summary = "프로필 사진 수정 API", description = "자신의 프로필 사진을 수정합니다.")
   ResponseEntity<ApiData<MemberDTO.Response>> updateProfilePicture(@RequestPart(value="image",required = false) MultipartFile image);
 
+  @Operation(summary = "멤버 Tag 가져오기 API", description = "로그인한 멤버의 Tag를 가져옵니다.")
+  ResponseEntity<ApiData<MemberTagGetResponse>> getMemberTag();
 }
---
+++ src/main/java/com/leets/team2/xclone/domain/member/dto/responses/MemberTagGetResponse.java
@@ -0,0 +1,7 @@
+package com.leets.team2.xclone.domain.member.dto.responses;
+
+public record MemberTagGetResponse(
+    String tag
+) {
+
+}
